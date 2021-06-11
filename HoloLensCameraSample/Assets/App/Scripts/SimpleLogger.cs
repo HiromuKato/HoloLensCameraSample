b3f180eda8c57c253f30b70a66e9eb4a8eedd757
HiromuKato <hiromu1212@gmail.com>
2021-06-11 10:56:47 +0900

    WebCamTexture を利用したサンプルシーン実装

---
+++ HoloLensCameraSample/Assets/App/Scripts/SimpleLogger.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace HoloLensCameraSample
+{
+    /// <summary>
+    /// シンプルなログ表示クラス
+    /// </summary>
+    public class SimpleLogger : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMesh textMesh;
+
+        private int lineCount = 0;
+
+        private void OnEnable()
+        {
+            textMesh.text = "";
+            Application.logMessageReceived += OnReceiveLog;
+        }
+
+        private void OnDestroy()
+        {
+            Application.logMessageReceived -= OnReceiveLog;
+        }
+
+        /// <summary>
+        /// ログを受け取った時の処理
+        /// </summary>
+        /// <param name="logText"></param>
+        /// <param name="stackTrace"></param>
+        /// <param name="logType"></param>
+        private void OnReceiveLog(string logText, string stackTrace, LogType logType)
+        {
+            // 100行を超えたら消す
+            lineCount++;
+            if (lineCount > 100)
+            {
+                lineCount = 0;
+                textMesh.text = "";
+            }
+
+            if (logType == LogType.Error || logType == LogType.Exception)
+            {
+                var colBegin = "<color='red'>";
+                var colEnd = "</color>";
+                textMesh.text += colBegin + logText + colEnd + "\n";
+            }
+            else if (logType == LogType.Warning)
+            {
+                var colBegin = "<color='yellow'>";
+                var colEnd = "</color>";
+                textMesh.text += colBegin + logText + colEnd + "\n";
+            }
+            else
+            {
+                textMesh.text += logText + "\n";
+            }
+        }
+    }
+}
---
+++ HoloLensCameraSample/Assets/App/Scripts/WebCamTextureSample.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HoloLensCameraSample
+{
+    /// <summary>
+    /// WebCamTexture を利用したサンプル
+    /// ドキュメント：https://docs.unity3d.com/ja/current/ScriptReference/WebCamTexture.html
+    /// </summary>
+    public class WebCamTextureSample : MonoBehaviour
+    {
+        [SerializeField]
+        private RawImage rawImage;
+
+        private WebCamTexture webCamTexture;
+
+        private void Start()
+        {
+            var w = 896;
+            var h = 504;
+
+            // Webカメラの取得
+            WebCamDevice userCameraDevice = WebCamTexture.devices[0];
+            webCamTexture = new WebCamTexture(userCameraDevice.name, w, h, 15);
+            rawImage.texture = webCamTexture;
+
+            Debug.Log($"DeviceName: {webCamTexture.deviceName}");
+            Debug.Log($"TextureSize: {webCamTexture.width} x {webCamTexture.height}");
+        }
+
+        private void OnDestroy()
+        {
+            webCamTexture.Stop();
+        }
+
+        public void StartWebCam()
+        {
+            webCamTexture.Play();
+            Debug.Log($"DeviceName: {webCamTexture.deviceName}");
+            Debug.Log($"WebCamTexuture: {webCamTexture.width} x {webCamTexture.height}");
+        }
+
+        public void StopWebCam()
+        {
+            webCamTexture.Stop();
+        }
+    }
+}
